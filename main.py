1a61e3e44ab97c6d24b2e1dffe21d0f2ce0da02a
Jonathan Conrad <jonathan@jonathans-mbp.udayton.edu>
2017-11-15 10:57:16 -0500

    Major changes to main. Use WebDriver wait element to monitor for captcha. Use headless Chrome as browser. Use file proxies.txt to source proxies or the command line. Fix error with copying cookies to new session. Restart thread 10 minutes after success (time to checkout).

--- main.py
+++ main.py
@@ -1,55 +1,97 @@
 from selenium import webdriver
-import os
-import requests
-import bs4
-import re
-import selenium.webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import RandomHeaders
 import threading
+import time
 import sys
-URL = sys.argv[2]
-PROXIES = sys.argv[2:]
-SPLASHTITLE = grabCurrentTitle(URL)
-class bot(object):
-	def __init__(self, proxy):
-		print('Initiated')
-		self.proxy = proxy
-		self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-		self.driver = webdriver.PhantomJS(service_args=['--proxy={}'.format(proxy), '--proxy-type=http'])
-		self.driver.get('https://www.reddit.com/r/cscareerquestions/')
+import os
 
-def grabCurrentTitle(url):
-	#this grabs the title of the splash page
-	driver = webdriver.PhantomJS()
-	driver.get(url)
-	title = driver.title
-	driver.close()
-	driver.quit()
-	return title
+# chrome option to prevent the window from closing after script completion - allow user to checkout #
+opts_co = webdriver.ChromeOptions()
+opts_co.add_experimental_option("detach", True)
+
+# set environment variable to use drivers
+chromedriver = os.path.dirname(__file__) + '/drivers/chromedriver'
+os.environ["webdriver.chrome.driver"] = chromedriver
+phantomjs = os.path.dirname(__file__) + '/drivers/phantomjs'
+os.environ["phantomjs.binary.path"] = phantomjs
+
+URL = sys.argv[1]
+# if proxies provided in argv uses those - otherwise read proxies from file
+if len(sys.argv) > 2:
+	PROXIES = sys.argv[2:]
+else:
+	PROXIES = [line.rstrip('\n') for line in open('proxies.txt')]
+
+
+def captcha_present(wait):
+	try:
+		# explicit wait - continually poll DOM looking for a captcha element (indicating splash bypass)
+		wait.until(ec.visibility_of_element_located(("class name", "g-recaptcha")))
+		return True
+	except TimeoutException as tex:
+		# captcha hasn't been found yet return false
+		return False
+
 
 def grabSS(proxy):
 	while True:
 		try:
-			headers = RandomHeaders.LoadHeader()
-			driver = webdriver.PhantomJS(service_args=['--proxy={}'.format(proxy), '--proxy-type=https'])
-			#driver = webdriver.PhantomJS()
+			opts = webdriver.ChromeOptions()
+			opts.add_argument('headless')
+			opts.add_argument("start-maximized")
+			opts.add_argument("--proxy-server=" + str(proxy))
+			opts.add_argument("user-agent=" + str(RandomHeaders.LoadHeader()))
+			driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=opts)
+			# implicitly retry polling DOM for 10 seconds before throwing an exception - website can lag during drop
+			driver.implicitly_wait(10)
+			# create a WebDriverWait object - explicit 60 second timeout
+			wait = WebDriverWait(driver, 5)
+
+			# load the splash page
 			driver.get(URL)
-			while driver.title == SPLASHTITLE:
-				driver.save_screenshot('{}.png'.format(proxy.replace(':', '').replace('.', '')))
-				#this just visualized the phantomjs driver - you can replace this with pass if you're trying to reduce mem
+
+			# wait until captcha found
+			while not captcha_present(wait):
+				print proxy + '\tstill waiting'
+				driver.get_screenshot_as_file('{}.png'.format(proxy.replace(':', '').replace('.', '')))
+
+			# splash page passed - save session cookies
+			print proxy + '\tPASSED SPLASH PAGE!'
 			cookies_list = driver.get_cookies()
 			driver.close()
 			driver.quit()
-			driver = webdriver.Firefox(service_args=['--proxy={}'.format(proxy), '--proxy-type=https'])
-			for cookie in cookies_list:
-				driver.add_cookie(cookie)
-				#converts phantomjs cookies into firefox webdriver to check out
+
+			# create a new driver for checkout
+			opts_co.add_argument("--proxy-server=" + str(proxy))
+			opts.add_argument("user-agent=" + str(RandomHeaders.LoadHeader()))
+			driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=opts_co)
+
+			# you can only set cookies for the driver's current domain so visit the page first then set cookies
 			driver.get(URL)
+			# precautionary - delete all cookies first
+			driver.delete_all_cookies()
+			for cookie in cookies_list:
+				# precautionary - prevent possible Exception - can only add cookie for current domain
+				if "adidas" in cookie['domain']:
+					driver.add_cookie(cookie)
+			# once cookies are changed browser must be refreshed
+			driver.refresh()
 
-
+			# this IP can re-enter the queue after the 10 minute checkout period
+			# to do so - simply wait for the 10 minute cart time to finish with an explicit sleep
+			time.sleep(60 * 10)
 		except Exception as exp:
-			print exp
+			print(exp)
+			driver.close()
+			driver.quit()
+
+
 if __name__ == "__main__":
+	print PROXIES
 	threads = [threading.Thread(target=grabSS, args=(proxy,)) for proxy in PROXIES]
 	for thread in threads:
 		thread.start()
